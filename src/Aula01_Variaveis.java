6997e53ab367f386106def2016609cb1331dcfc7
Colt33 <wellingtonrcampos@hotmail.com>
2025-10-10 00:06:21 -0300

    teste

---
+++ src/Aula01_Variaveis.java
@@ -0,0 +1,15 @@
+public class Aula01_Variaveis {
+    /*Variavel é todo dado que for alocado na memoria.
+     *Quando for criar uma variavel é necessario definir o tipo da variavel.
+     *
+     * Ex01: int num = 5;
+     *   O "int" é o tipo da variavel,
+     *   O "num" é o nome da variavel,
+     *   O "5" é a variavel.
+     *
+     *  Ex02: String name = "Colt";
+     *   O "String" é o tipo da variavel,
+     *   O "name" é o nome da variavel,
+     *   O "Colt" é a variavel.
+     * */
+}
