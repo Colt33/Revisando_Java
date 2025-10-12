3f3d39870fbb1f3cc7678b574564dadd5e636df1
Colt33 <wellingtonrcampos@hotmail.com>
2025-10-12 20:41:57 -0300

    Aula 02

---
+++ src/Aula02_TiposPrimitivos.java
@@ -0,0 +1,25 @@
+public class Aula02_TiposPrimitivos {
+    public static void main(String[] args) {
+
+        //Numeros Inteiros = 100, 234345, 255, 120, 1, 919393939939394
+
+        //byte => 8 bits => -128 a 127
+        //short => 16 bits => -32.768 a 32.767
+        //int => 32 bits => -2.147.483.648 a 2.147.483.647
+        //long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        //Numeros Decimais =1.2, 3.4, 5.6, 7.8, 9.0
+        //5.50
+        //float => 32 bits => 1.4e-45 a 3.4028235e+38
+        //float => 32 bits => precisão simples
+        //double => 64 bits => precisão dupla
+
+        //String => Será usada para escrita, representar palavras e frases
+        /*char => Será usada para um unico caractere => 'a', 'b', 'c', 'd', =>
+         * char usa aspas simples */
+
+        //boolean => True or False
+        //dsfsdsv
+
+    }
+}
