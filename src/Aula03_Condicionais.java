22bbeb915357e797f78d2b45ca1b1fb356111a8c
Colt33 <wellingtonrcampos@hotmail.com>
2025-10-12 20:54:41 -0300

    Aula03

---
+++ src/Aula03_Condicionais.java
@@ -0,0 +1,12 @@
+public class Aula03_Condicionais {
+    public static void main(String[] args) {
+
+        short b = 10;
+
+        if (b > 99){
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("False");
+        }
+    }
+}
