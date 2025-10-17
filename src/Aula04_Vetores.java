cec3629ea16e347a677083c0a1a07c32a1c6709c
Colt33 <wellingtonrcampos@hotmail.com>
2025-10-16 23:59:17 -0300

    Vetores arrays

--- src/Aula04_Vetores.java
+++ src/Aula04_Vetores.java
@@ -1,2 +1,16 @@
 public class Aula04_Vetores {
+    public static void main(String[] args) {
+
+        int[] Inteiros = {1, 2, 3, 4, 5, 6};
+        int[] soma = {0, 1, 3, 5};
+        int sow = soma[3] + Inteiros[4];
+        if (Inteiros[5] > 0){
+            ++soma[3];
+            System.out.println(soma[3]);
+        } else {
+                    System.out.println(Inteiros[0]);
+            }
+
+
+    }
 }
